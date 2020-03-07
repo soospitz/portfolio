9efc6296e743dbfd9e867dc540e3f4efe6469c11
soospitz <sks568@nyu.edu>
2020-03-07 18:36:57 -0500

    added more projects 2020

---
+++ portfolio-extra/SimpleTable_fall2019/tableTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tableTest
+//
+///  Created by Nathan Hull
+//  Copyright (c) 2019 NYU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
